b341d025e7687b63d45d3d1b1ae113970a3a5e51
ting.yan2 <ting.yan2@oyohotels.cn>
2019-08-18 16:59:39 +0800

    fix count-down

--- count-down/index.js
+++ count-down/index.js
@@ -1,23 +1,4 @@
 let btn = document.getElementById('btn');
-const TEXT = btn.innerHTML
-
-/*
- * 倒计时
- * @ele: 要增加倒计时的元素
- * @time: 倒计时的时间
- * @text：倒计时结束后要显示的内容
- */
-function countDown(ele, time, text) {
-    let timer;
-    let second = time;
-    timer = setInterval(() => {
-        second -= 1;
-        ele.innerHTML = second;
-        if (second < 0) {
-            ele.innerHTML = text;
-            clearInterval(timer);
-        }
-    }, 1000);
-}
+const TEXT = btn.innerHTML;
 
 btn.addEventListener('click', () => countDown(btn, 5, TEXT));
